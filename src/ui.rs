9c4fe03f72c23fa49cc4d01c7b1aa8dc7360f0b5
Alex Thorne <alex@thorne.dev>
2022-10-16 12:04:14 +0100

    add auto arrange windows button to context menu. #52

--- src/ui.rs
+++ src/ui.rs
@@ -1,4 +1,5 @@
-use egui::{Align, Checkbox, Context, Layout, Sense};
+use egui::{Align, Checkbox, Context, Frame, Layout, Sense};
+use egui::style::Margin;
 
 use chipper8::machine::Machine;
 use windows::Window;
@@ -29,20 +30,29 @@ impl Ui {
         egui::CentralPanel::default().show(
             ctx, |_ui| {},
         ).response.interact(Sense::click()).context_menu(|ui| {
-            ui.with_layout(Layout::top_down(Align::Center), |ui| {
-                ui.heading("Windows");
-            });
-            ui.separator();
-            for window in &mut self.windows {
-                let label_text = window.name();
-                let checkbox = Checkbox::new(&mut window.open, label_text);
-                if ui.add(checkbox).clicked() {
-                    ui.close_menu();
-                }
-            }
+            window_menu_ui(ui, &mut self.windows)
         });
         for window in &mut self.windows {
             window.draw(ctx, machine, state);
         }
+    }
+}
+
+fn window_menu_ui(ui: &mut egui::Ui, windows: &mut Vec<Window>) {
+    ui.with_layout(Layout::top_down(Align::Center), |ui| {
+        ui.heading("Windows");
+    });
+    ui.separator();
+    for window in windows {
+        let label_text = window.name();
+        let checkbox = Checkbox::new(&mut window.open, label_text);
+        if ui.add(checkbox).clicked() {
+            ui.close_menu();
+        }
+    }
+    ui.separator();
+    if ui.button("Auto-arrange").clicked() {
+        ui.ctx().memory().reset_areas();
+        ui.close_menu();
     }
 }
