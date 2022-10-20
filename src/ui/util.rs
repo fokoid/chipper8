496abf6a544ed8a778470bfcd50420ad95593949
Alex Thorne <alex@thorne.dev>
2022-10-20 13:13:47 +0100

    remove obsolete `MonoLabel` type

--- src/ui/util.rs
+++ src/ui/util.rs
@@ -1,7 +1,4 @@
-use std::fmt::{Debug, Formatter};
-
-use egui::{Color32, Label, Response, TextEdit, Ui, Widget};
-use egui::widget_text::RichText;
+use egui::{Response, TextEdit, Ui};
 
 pub use formatting::{Address, Byte, Decimal, Nibble, Register, Word};
 pub use memory_display::MemoryDisplay;
@@ -13,33 +10,6 @@ mod image_builder;
 mod memory_display;
 pub mod table;
 mod formatting;
-
-pub struct MonoLabel {
-    text: String,
-    background_color: Option<Color32>,
-}
-
-impl MonoLabel {
-    pub fn new(text: impl Into<String>) -> Self {
-        Self { text: text.into(), background_color: None }
-    }
-}
-
-impl Debug for MonoLabel {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.text)
-    }
-}
-
-impl Widget for MonoLabel {
-    fn ui(self, ui: &mut Ui) -> Response {
-        let mut text = RichText::new(self.text).monospace().size(16.0);
-        if let Some(background_color) = self.background_color {
-            text = text.background_color(background_color);
-        };
-        ui.add(Label::new(text))
-    }
-}
 
 /// helper function to wrap textbox add with global key capture management
 pub fn add_text_edit(ui: &mut Ui, state: &mut State, widget: TextEdit) -> Response {
