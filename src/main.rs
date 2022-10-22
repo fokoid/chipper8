1034a6ca3be95e68de5ef9917dc93d65d277c4ad
Alex Thorne <alex@thorne.dev>
2022-10-22 15:48:17 +0100

    headless mode for emulator #97

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::path::PathBuf;
-use std::time::Duration;
+use std::time::{Duration, Instant};
+use std::thread;
 
 use clap::Parser;
 use eframe::NativeOptions;
@@ -13,11 +14,18 @@ use chipper8::ui::windows::Display;
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
+    // todo: split up GUI and emulator args
     #[arg(index = 1)]
     rom: PathBuf,
 
     #[arg(short, long, default_value_t = 60)]
     fps: u64,
+
+    #[arg(long, default_value_t = false)]
+    headless: bool,
+
+    #[arg(long)]
+    dump: Option<PathBuf>,
 }
 
 impl Args {
@@ -29,52 +37,56 @@ impl Args {
 fn main() -> Result<()> {
     let mut args = Args::parse();
     args.rom.set_extension("rom");
-    let mut native_options = NativeOptions::default();
-    native_options.resizable = true;
-    native_options.initial_window_size = Some(Vec2 { x: 280.0, y: 140.0 });
-    eframe::run_native("CHIPPER-8", native_options,
-                       Box::new(|cc| Box::new(EmulatorApp::new(cc, args))));
+    let mut emulator = Emulator::new(args)?;
+    if !emulator.args.headless {
+        let mut native_options = NativeOptions::default();
+        native_options.resizable = true;
+        native_options.run_and_return = false;
+        native_options.initial_window_size = Some(Vec2 { x: 280.0, y: 140.0 });
+        eframe::run_native("CHIPPER-8", native_options,
+                           Box::new(|cc| Box::new(EmulatorApp::new(cc, emulator))));
+    } else {
+        // no display: useful for testing when combined with state dump
+        while !emulator.terminated {
+            emulator.tick();
+            thread::sleep(emulator.args.frame_time());
+        }
+        if let Some(dump) = &emulator.args.dump {
+            eprintln!("Writing final machine state to '{}'", dump.display());
+            todo!("Implement serde for machine");
+        }
+    }
     Ok(())
 }
 
-struct EmulatorApp {
+struct Emulator {
     machine: Machine,
-    last_time: f64,
-    display: Display,
-    args: Args,
-    key_capture: KeyCapture,
+    last_time: Instant,
     terminated: bool,
+    args: Args,
 }
 
-impl EmulatorApp {
-    fn new(_cc: &eframe::CreationContext<'_>, args: Args) -> Self {
+impl Emulator {
+    fn new(args: Args) -> Result<Self> {
         let mut rom = Rom::from_file(&args.rom).unwrap();
         let mut machine = Machine::new();
         machine.load_rom(&mut rom, 0x200);
         println!("CHIPPER-8: running ROM '{}'.", rom.name);
-        Self {
+        Ok(Self {
             machine,
-            last_time: 0.0,
-            display: Display::minimal(),
-            args,
-            key_capture: KeyCapture::new(),
+            last_time: Instant::now(),
             terminated: false,
-        }
+            args,
+        })
     }
-}
 
-impl eframe::App for EmulatorApp {
-    fn update(&mut self, ctx: &Context, _frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(
-            ctx, |ui| {
-                self.display.ui_stateless(ui, &mut self.machine);
-                self.key_capture.update(ui);
-            },
-        );
-        if self.terminated { return; }
-        self.machine.key_buffer = self.key_capture.key();
-        if ctx.input().time - self.last_time > self.args.frame_time().as_secs_f64() {
-            self.last_time = ctx.input().time;
+    fn tick(&mut self) {
+        if self.terminated {
+            return;
+        }
+        let current_time = Instant::now();
+        if current_time - self.last_time > self.args.frame_time() {
+            self.last_time = current_time;
             match self.machine.next_instruction() {
                 Ok(instruction) => {
                     println!("Executing: {}", instruction);
@@ -94,6 +106,36 @@ impl eframe::App for EmulatorApp {
                 _ => {}
             }
         }
-        ctx.request_repaint_after(self.args.frame_time());
+    }
+}
+
+struct EmulatorApp {
+    emulator: Emulator,
+    display: Display,
+    key_capture: KeyCapture,
+}
+
+impl EmulatorApp {
+    fn new(_cc: &eframe::CreationContext<'_>, emulator: Emulator) -> Self {
+        Self {
+            emulator,
+            display: Display::minimal(),
+            key_capture: KeyCapture::new(),
+        }
+    }
+
+}
+
+impl eframe::App for EmulatorApp {
+    fn update(&mut self, ctx: &Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(
+            ctx, |ui| {
+                self.display.ui_stateless(ui, &mut self.emulator.machine);
+                self.key_capture.update(ui);
+            },
+        );
+        self.emulator.machine.key_buffer = self.key_capture.key();
+        self.emulator.tick();
+        ctx.request_repaint_after(self.emulator.args.frame_time());
     }
 }
